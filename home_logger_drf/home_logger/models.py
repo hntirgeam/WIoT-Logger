3a42da54ff7ccbd53b1d7e6335a8b99f7a376c17
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 11:52:34 +0300

    models

--- home_logger_drf/home_logger/models.py
+++ home_logger_drf/home_logger/models.py
@@ -7,19 +7,25 @@ from django.utils import timezone
 from django.db.models.deletion import CASCADE, DO_NOTHING
 
 class Device(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE, related_name="device")
-    name = models.CharField(max_length=32, null=True, default=None)
+    owner = models.ForeignKey(User, on_delete=CASCADE, related_name="devices")
+    name = models.CharField(max_length=32)
     description = models.CharField(max_length=256, null=True, default=None)
     mac_address = models.CharField(max_length=20, null=True, default=None)
     ip_address = models.GenericIPAddressField()
     date_added = models.DateTimeField(default=timezone.now)
+    
+    class Meta:
+        ordering = ['date_added']
 
 class Record(models.Model):
-    device = models.ForeignKey(Device, on_delete=DO_NOTHING, related_name="record")
+    device = models.ForeignKey(Device, on_delete=DO_NOTHING, related_name="records")
     temp = models.DecimalField(max_digits=8, decimal_places=4, default=Decimal(0))
     humidity = models.DecimalField(max_digits=8, decimal_places=4, default=Decimal(0))
     pressure = models.DecimalField(max_digits=8, decimal_places=4, default=Decimal(0))
     CO2 = models.DecimalField(max_digits=8, decimal_places=4, default=Decimal(0))
     eTVOC = models.DecimalField(max_digits=8, decimal_places=4, default=Decimal(0))
     timestamp = models.DateTimeField(default=timezone.now)
+    
+    class Meta:
+        ordering = ['timestamp']
     
