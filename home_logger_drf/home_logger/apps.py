a24543f626f08b98535df1356917858fd831d6da
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 08:55:32 +0300

    updated appconfig.name for makemigrations

--- home_logger_drf/home_logger/apps.py
+++ home_logger_drf/home_logger/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HomeLoggerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home_logger'
+    name = 'home_logger_drf.home_logger'
