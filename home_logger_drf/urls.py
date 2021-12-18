7c74df6d12a4b16a4dc3320ec979a6eda01bdb52
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 13:42:24 +0300

    url upd

--- home_logger_drf/urls.py
+++ home_logger_drf/urls.py
@@ -5,8 +5,7 @@ from home_logger_drf.home_logger import views
 urlpatterns = [
     path('devices/', views.DeviceList.as_view()),
     path('devices/<int:pk>/', views.DeviceDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('me', views.UserDetail.as_view()),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
     path('api-auth/', include('rest_framework.urls')),
