726ab30ab5294e77daaeb66395e53e44078d3f4b
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 20:50:14 +0300

    serializers reworked

--- home_logger_drf/home_logger/serializers.py
+++ home_logger_drf/home_logger/serializers.py
@@ -1,20 +1,42 @@
 from rest_framework import serializers
-
+from rest_framework.fields import SerializerMethodField
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
 from home_logger_drf.home_logger import models
 
 
 
 class UserSerializer(serializers.ModelSerializer):
-    devices = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Device.objects.all())
+    devices_amount = SerializerMethodField()
 
     class Meta:
         model = models.User
-        fields = ['id', 'username', 'devices']
+        fields = ['id', 'username', 'devices_amount']
+        
+    def get_devices_amount(self, obj):
+        return models.Device.objects.filter(owner=obj).count()
 
 class DeviceSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
     class Meta:
         model = models.Device
-        fields = ['owner', 'name', 'description', 'mac_address', 'ip_address']
-        extra_kwargs = {"user": {"required": False}}
+        fields = ['owner', 'id', 'name', 'description', 'ip_address', 'uuid']
+        extra_kwargs = {"ip_address": {"required": False}}
+        
+    def create(self, validated_data):
+        user = self.context['request'].user
+        device = models.Device.objects.create(owner=user, **validated_data)
+        return device
+
+
+class RecordSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = models.Record
+        fields = ['id', 'temp', 'humidity', 'pressure', 'CO2', 'eTVOC', 'timestamp']
+        
+    def create(self, validated_data):
+        uuid = self.context['request'].data["uuid"]
+        device = get_object_or_404(models.Device, uuid=uuid)
+        record = models.Record.objects.create(device=device, **validated_data)
+        return record
