8984c24adf149d3175692a32029b86937abc4ac2
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 11:52:59 +0300

    views

--- home_logger_drf/home_logger/views.py
+++ home_logger_drf/home_logger/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from home_logger_drf.home_logger import models
+from home_logger_drf.home_logger import serializers
+from home_logger_drf.home_logger.permissions import IsOwner
 
-# Create your views here.
+from rest_framework import permissions
+from rest_framework import generics
+
+
+class UserList(generics.ListAPIView):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+    
+    
+class DeviceList(generics.ListCreateAPIView):
+    queryset = models.Device.objects.all()
+    serializer_class = serializers.DeviceSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class DeviceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Device.objects.all()
+    serializer_class = serializers.DeviceSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
