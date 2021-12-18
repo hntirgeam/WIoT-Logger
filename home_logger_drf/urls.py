8fbee9ff3bcac2ad31b00f95fc649aaf8dcbd5b9
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 20:48:39 +0300

    reworked urls for mixines

--- home_logger_drf/urls.py
+++ home_logger_drf/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from home_logger_drf.home_logger import views
 
+router = DefaultRouter(trailing_slash=True)
+router.register(r"users", views.UserViewSet)
+router.register(r"devices", views.DeviceViewSet)
+router.register(r"record", views.RecordViewSet)
+
+
 urlpatterns = [
-    path('devices/', views.DeviceList.as_view()),
-    path('devices/<int:pk>/', views.DeviceDetail.as_view()),
-    path('me', views.UserDetail.as_view()),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
     path('api-auth/', include('rest_framework.urls')),
-]
+] + router.urls
