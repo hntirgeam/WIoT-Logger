0f61fc221a38933c7d61cfdfcf5a8430a48e9b28
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 12:40:12 +0300

    djoser added

--- home_logger_drf/settings.py
+++ home_logger_drf/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    
     'home_logger_drf.home_logger',
 ]
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
