757385ace532a5710c369bd6d06efdba98391335
hntirgeam <kmacroviperaz@gmail.com>
2021-12-18 11:52:54 +0300

    serializers

---
+++ home_logger_drf/home_logger/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from home_logger_drf.home_logger import models
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    devices = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Device.objects.all())
+
+    class Meta:
+        model = models.User
+        fields = ['id', 'username', 'devices']
+
+class DeviceSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
+    class Meta:
+        model = models.Device
+        fields = ['owner', 'id', 'name', 'description', 'mac_address', 'ip_address']
+        extra_kwargs = {"user": {"required": False}}
