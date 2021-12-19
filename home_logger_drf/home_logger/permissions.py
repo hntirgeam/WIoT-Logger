8a7d84111d93f44926f96b88a3d1b5cf01cb4894
hntirgeam <kmacroviperaz@gmail.com>
2021-12-19 16:24:16 +0300

    new permission for csv

--- home_logger_drf/home_logger/permissions.py
+++ home_logger_drf/home_logger/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+class IsDeviceOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
+    
+class IsCSVOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        device_pk = int(view.kwargs.get('pk', None))
+        return device_pk in [i.id for i in request.user.devices.all()]
